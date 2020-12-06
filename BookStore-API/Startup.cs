24d338afd5f93f921ddbcd058608b7b6d3cdf375
Guillermo <guillermo.perez@impac.co.nz>
2020-12-06 15:08:03 +1300

    Created Data Classes and DTOs. Also included automapper and chage appsettings file.

--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using BookStore_API.Contracts;
 using BookStore_API.Data;
+using BookStore_API.Mappings;
 using BookStore_API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -46,6 +48,8 @@ namespace BookStore_API
           .AllowAnyMethod()
           .AllowAnyHeader());
       });
+
+      services.AddAutoMapper(typeof(Maps));
 
       services.AddSwaggerGen(c => {
         c.SwaggerDoc("v1", new OpenApiInfo { 
